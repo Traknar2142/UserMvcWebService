84a52e0e321af9c53d199f27679cb904375c371d
Traknar <Traknar@DESKTOP-3M51OS8>
2021-03-08 18:41:19 +0300

    Edit mapping methods

--- UserMvcWebService/src/main/java/ru/diasoft/user_mvc_webservice/controllers/UserController.java
+++ UserMvcWebService/src/main/java/ru/diasoft/user_mvc_webservice/controllers/UserController.java
@@ -3,15 +3,16 @@ package ru.diasoft.user_mvc_webservice.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ru.diasoft.user_mvc_webservice.entities.User;
-import ru.diasoft.user_mvc_webservice.exceptions.UserNotFoundException;
 import ru.diasoft.user_mvc_webservice.services.UserService;
 
 @RestController
@@ -37,12 +38,12 @@ public class UserController {
         userService.add(user);
     }
 
-    @PostMapping("/update")
+    @PutMapping("/update")
     public void updateUser(@RequestBody User user) {
         userService.update(user);
     }
 
-    @GetMapping("/delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public void deleteUser(@PathVariable("id") int id) {
         userService.removeUser(id);
     }
